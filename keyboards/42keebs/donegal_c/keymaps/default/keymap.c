5493db47f5261a042834de998ff7f5f25a4e5017
Troels Lenda Strand <troelslenda@gmail.com>
2023-02-16 21:22:14 +0100

    Adding changes from RarePotato8DE

--- keyboards/42keebs/donegal_c/keymaps/default/keymap.c
+++ keyboards/42keebs/donegal_c/keymaps/default/keymap.c
@@ -12,42 +12,30 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/keymap_br_abnt2.h
- https://github.com/qmk/qmk_firmware/issues/6325 SEE HERE IF COMPILE FAILS
 */
-//#include QMK_KEYBOARD_H
 #include "donegal_c.h"
-#include "keymap_br_abnt2.h"
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	LAYOUT_60_abnt(
-		KC_Z, KC_2, KC_3, KC_5, KC_7, KC_9, KC_MINS, KC_BSPC, 
-		KC_CAPS, KC_1, KC_4, KC_6, KC_8, KC_0, KC_EQL, KC_BSPC, 
-		KC_TAB, KC_Q, KC_E, KC_T, KC_U, KC_O, BR_ACUT, BR_LBRC, 
-		BR_QUOT, KC_W, KC_R, KC_Y, KC_I, KC_P, KC_SCLN, KC_ENT, 
-		KC_LSFT, KC_A, KC_D, KC_G, KC_J, KC_L, BR_TILD, BR_BSLS, 
-		BR_RBRC, KC_S, KC_F, KC_H, KC_K, KC_COMM, BR_SCLN, BR_CCDL, 
-		KC_LCTL, KC_X, KC_C, KC_B, KC_N, KC_DOT, KC_RGUI, KC_RCTL, 
-		KC_LGUI, KC_LALT, KC_V, KC_SPC, KC_M, KC_RALT, KC_MENU, KC_RSFT),
 
-	LAYOUT_60_abnt(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[0] = LAYOUT_all(
+  KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,       KC_6,      KC_7,      KC_8,      KC_9,       KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
+  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,       KC_Y,      KC_U,      KC_I,      KC_O,       KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,       KC_H,      KC_J,      KC_K,      KC_L,       KC_SCLN, KC_QUOT,          KC_ENT,
+  KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,       KC_B,      KC_N,      KC_M,      KC_COMM,    KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT,
+  KC_LCTL,  KC_LGUI,  KC_LALT,            KC_SPC,   KC_SPC,     KC_SPC,               KC_RALT,   KC_RGUI,    KC_APP,  KC_RCTL),
+
+[1] = LAYOUT_all(
+  KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,      KC_F10,  KC_F11,   KC_F12,  KC_TRNS, KC_TRNS,
+  KC_TRNS,  KC_TRNS,  KC_UP,    KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS,           KC_TRNS,
+  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS)
 };
 
-void matrix_init_user(void) {
-  //user initialization
-}
 
-void matrix_scan_user(void) {
-  //user matrix
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    oled_write_P(PSTR("42keebs"), false);
+    return false;
 }
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
-}
+#endif
