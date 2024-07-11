2f211d2267dfedbbc6fd77b22d7bb95ac18d220f
Troels Lenda Strand <troelslenda@gmail.com>
2024-07-11 21:44:36 +0200

    Tweak donegal_c and add custom keymap

--- keyboards/42keebs/donegal_c/keymaps/default/rules.mk
+++ keyboards/42keebs/donegal_c/keymaps/default/rules.mk
@@ -3,22 +3,15 @@ MCU = atmega328p
 BOOTLOADER = usbasploader
 
 BOOTMAGIC_ENABLE = yes
-MOUSEKEY_ENABLE = yes
+MOUSEKEY_ENABLE = no
 EXTRAKEY_ENABLE = yes
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 NKRO_ENABLE = no
-
-TAP_DANCE_ENABLE = yes
-#WPM_ENABLE = yes
 
 
 OLED_ENABLE = yes
 OLED_DRIVER_ENABLE = yes
 OLED_DRIVER = SSD1306
 
-#ifneq ($(strip $(NO_SECRETS)), yes)
-#    ifneq ("$(wildcard $(USER_PATH)/secrets.c)","")
-#        SRC += secrets.c
-#    endif
-#endif
+LAYOUTS = all
