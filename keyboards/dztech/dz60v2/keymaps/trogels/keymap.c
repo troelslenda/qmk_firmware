a6d2ca9bf6139e05f831b6545b92051536f3ff0c
Troels Lenda Strand <tsd@bankdata.dk>
2025-02-17 17:09:02 +0100

    Updates

--- keyboards/dztech/dz60v2/keymaps/trogels/keymap.c
+++ keyboards/dztech/dz60v2/keymaps/trogels/keymap.c
@@ -2,7 +2,7 @@
 #include QMK_KEYBOARD_H
 //#include "quantum.h"
 // tap dance declarations
-enum tap_dance { _TD_ESC, _TD_LCTRL, _TD_RCTRL };
+enum tap_dance { TD_ESC, TD_LCTL, TD_RCTL };
 
 enum custom_keycodes {
     C_MAIL = SAFE_RANGE,
@@ -12,13 +12,19 @@ enum custom_keycodes {
     
 };
 
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_ESC]   = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_GRV),
+    [TD_LCTL] = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_BTN4),
+    [TD_RCTL] = ACTION_TAP_DANCE_DOUBLE(KC_RCTL, KC_BTN5)};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT_all(
-        QK_GESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,        KC_BSPC,
+        TD(TD_ESC),   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,        KC_BSPC,
         KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_CAPS,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,       KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_RSFT,      KC_NO,
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_NO,   KC_SPC,  KC_NO,            KC_RALT, MO(1),   KC_NO, KC_APP,    KC_RCTL
+        TD(TD_LCTL), KC_LGUI, KC_LALT,                   KC_NO,   KC_SPC,  KC_NO,            KC_RALT, MO(1),   KC_NO, KC_APP,    TD(TD_RCTL)
     ),
     LAYOUT_all(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
@@ -43,27 +49,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case KC_HELP:
         if (record->event.pressed) {
             // when keycode QMKBEST is pressed
             SEND_STRING("KEYBOARD INSTRUCTIONS\n######## ############\n");
+            SEND_STRING("\n");
+            SEND_STRING(",---,---,---,---,---,---,---,---,---,---,---,---,---,-------,\n");
+            SEND_STRING("| ~  | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | [ | ] | <-    |\n");
+            SEND_STRING("|---'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-----|\n");
+            SEND_STRING("| ->| | Q | W | E | R | T | Y | U | I | O | P | [ | ] |  \\  |\n");
+            SEND_STRING("|-----',--',--',--',--',--',--',--',--',--',--',--',--'-----|\n");
+            SEND_STRING("| caps | A | S | D | F | G | H | J | K | L | ; | '  |  enter |\n");
+            SEND_STRING("|------'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'--------|\n");
+            SEND_STRING("| shift  | Z | X | C | V | B | N | M | , | . | / |    shift |\n");
+            SEND_STRING("|------,-',--'--,'---'---'---'---'---'---'-,-'---',--,------|\n");
+            SEND_STRING("| ctl | win | alt |                 | alt | fn  | mnu | ctl |\n");
+            SEND_STRING("'-----'-----'-----'-----------------'-----'-----'-----'-----'\n");
+            SEND_STRING("\n");
+            SEND_STRING("FN layer\n");
+            SEND_STRING(",---,---,---,---,---,---,---,---,---,---,---,---,---,-------,\n");
+            SEND_STRING("| ~  |   |   |   |   | 5 | 6 | 7 | 8 | 9 | 0 | [ | ] | del  |\n");
+            SEND_STRING("|---'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-----|\n");
+            SEND_STRING("| ->| |   | ↑ |   |   |   | Y | U | I | O | P | [ | ] |  \\  |\n");
+            SEND_STRING("|-----',--',--',--',--',--',--',--',--',--',--',--',--'-----|\n");
+            SEND_STRING("| caps | ← | ↓ | → |   |   | H | J | K | L | ; | '  |kp enter|\n");
+            SEND_STRING("|------'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'--------|\n");
+            SEND_STRING("| shift  |   |   |   |   | B | N | M | , | . | / |    shift |\n");
+            SEND_STRING("|------,-',--'--,'---'---'---'---'---'---'-,-'---',--,------|\n");
+            SEND_STRING("| ctrl | supr | alt |               | alt | fn | mo2 | ctrl |\n");
+            SEND_STRING("'------'------'-----'---------------'-----'----'-----'------'\n");
 
-// ,---,---,---,---,---,---,---,---,---,---,---,---,---,-------,\n
-// | ~ | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | [ | ] | <-    |\n
-// |---'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-----|\n
-// | ->| | Q | W | E | R | T | Y | U | I | O | P | [ | ] |  \  |\n
-// |-----',--',--',--',--',--',--',--',--',--',--',--',--'-----|\n
-// | caps | A | S | D | F | G | H | J | K | L | ; | ' |  enter |\n
-// |------'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'--------|\n
-// | shift  | Z | X | C | V | B | N | M | , | . | / |    shift |\n
-// |------,-',--'--,'---'---'---'---'---'---'-,-'---',--,------|\n
-// | ctrl | supr | alt |               | alt | fn | mnu | ctrl |\n
-// '------'------'-----'---------------'-----'----'-----'------'\n
-                        
         } else {
             // when keycode QMKBEST is released
         }
@@ -71,6 +88,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 };
+
+
+
+// KEYBOARD INSTRUCTIONS
+// ######## ############
+
+// ,---,---,---,---,---,---,---,---,---,---,---,---,---,-------,
+// | ~ | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | [ | ] | <-    |
+// |---'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-----|
+// | ->| | Q | W | E | R | T | Y | U | I | O | P | [ | ] | . \  |
+// |-----',--',--',--',--',--',--',--',--',--',--',--',--'-----|
+// | caps | A | S | D | F | G | H | J | K | L | ; | ' |  enter |
+// |------'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'--------|
+// | shift  | Z | X | C | V | B | N | M | , | . | / |  .  shift |
+// |------,-',--'--,'---'---'---'---'---'---'-,-'---',--,------|
+// | ctrl | supr | alt |               | alt | fn | mnu | ctrl |
+// '------'------'-----'---------------'-----'----'-----'------''
+
 
 
 //enum layers { _BASE, _FN1, _FN2, _STANDARD, _MACOS, _DAYZ };
---
+++ keyboards/dztech/dz60v2/keymaps/trogels/rules.mk
@@ -0,0 +1,4 @@
+TAP_DANCE_ENABLE = yes
+#UNICODE_COMMON = yes
+#VIA_ENABLE = yes
+
